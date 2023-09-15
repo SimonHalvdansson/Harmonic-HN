5feaefaf2c64d7addce27beb1e75a493c0f564a7
Appearamid Guy <appearamidguy@gmail.com>
2023-09-15 17:29:48 +0000

    Clean up some warnings in VolleyOkHttp3StackInterceptors
    The only one left is about closing OkHttp Response. But getting the timing right require more research, so it will be handled in a different pr.

--- app/src/main/java/com/simon/harmonichackernews/network/VolleyOkHttp3StackInterceptors.java
+++ app/src/main/java/com/simon/harmonichackernews/network/VolleyOkHttp3StackInterceptors.java
@@ -24,6 +24,8 @@ import okhttp3.ResponseBody;
 
 public class VolleyOkHttp3StackInterceptors extends BaseHttpStack {
 
+    private static final RequestBody EMPTY_REQUEST = RequestBody.create(new byte[0]);
+
     public VolleyOkHttp3StackInterceptors() {
 
     }
@@ -36,7 +38,7 @@ public class VolleyOkHttp3StackInterceptors extends BaseHttpStack {
                 // Ensure backwards compatibility.  Volley assumes a request with a null body is a GET.
                 byte[] postBody = request.getBody();
                 if (postBody != null) {
-                    builder.post(RequestBody.create(MediaType.parse(request.getBodyContentType()), postBody));
+                    builder.post(RequestBody.create(postBody, MediaType.parse(request.getBodyContentType())));
                 }
                 break;
             case Request.Method.GET:
@@ -68,12 +70,14 @@ public class VolleyOkHttp3StackInterceptors extends BaseHttpStack {
         }
     }
 
-    private static RequestBody createRequestBody(Request r) throws AuthFailureError {
+    private static RequestBody createRequestBody(Request<?> r) throws AuthFailureError {
         final byte[] body = r.getBody();
         if (body == null) {
-            return null;
+            // For POST, PUT and PATCH requests Volley's HurlStack doesn't add body when it's null.
+            // However OkHttp requires non-null RequestBody for those methods, so use an empty body.
+            return EMPTY_REQUEST;
         }
-        return RequestBody.create(MediaType.parse(r.getBodyContentType()), body);
+        return RequestBody.create(body, MediaType.parse(r.getBodyContentType()));
     }
 
     @Override
@@ -89,11 +93,11 @@ public class VolleyOkHttp3StackInterceptors extends BaseHttpStack {
         okHttpRequestBuilder.url(request.getUrl());
 
         Map<String, String> headers = request.getHeaders();
-        for (final String name : headers.keySet()) {
-            okHttpRequestBuilder.addHeader(name, headers.get(name));
+        for (Map.Entry<String, String> header: headers.entrySet()) {
+            okHttpRequestBuilder.addHeader(header.getKey(), header.getValue());
         }
-        for (final String name : additionalHeaders.keySet()) {
-            okHttpRequestBuilder.addHeader(name, additionalHeaders.get(name));
+        for (Map.Entry<String, String> header: additionalHeaders.entrySet()) {
+            okHttpRequestBuilder.addHeader(header.getKey(), header.getValue());
         }
 
         setConnectionParametersForRequest(okHttpRequestBuilder, request);
@@ -101,6 +105,11 @@ public class VolleyOkHttp3StackInterceptors extends BaseHttpStack {
         OkHttpClient client = clientBuilder.build();
         okhttp3.Request okHttpRequest = okHttpRequestBuilder.build();
         Call okHttpCall = client.newCall(okHttpRequest);
+        // todo: close response. Note that it is not as simple as adding try-with-resources because
+        //  that would close the response before Volley has had a chance to consume it. At the same
+        //  time, not closing the response is also wrong because it will not be closed at all.
+        //  Volley closes only input stream created from response body's which is not the same as
+        //  closing the response.
         Response okHttpResponse = okHttpCall.execute();
 
 
@@ -116,9 +125,7 @@ public class VolleyOkHttp3StackInterceptors extends BaseHttpStack {
         List<Header> headers = new ArrayList<>();
         for (int i = 0, len = responseHeaders.size(); i < len; i++) {
             final String name = responseHeaders.name(i), value = responseHeaders.value(i);
-            if (name != null) {
-                headers.add(new Header(name, value));
-            }
+            headers.add(new Header(name, value));
         }
         return headers;
     }
