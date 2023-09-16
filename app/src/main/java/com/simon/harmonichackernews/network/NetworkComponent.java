fcd85f3a3ab81f1640198eab4e5d615b90098f07
Simon Halvdansson <simonhalvdansson@gmail.com>
2023-09-16 08:51:32 +0200

    Add user-agent

--- app/src/main/java/com/simon/harmonichackernews/network/NetworkComponent.java
+++ app/src/main/java/com/simon/harmonichackernews/network/NetworkComponent.java
@@ -1,13 +1,19 @@
 package com.simon.harmonichackernews.network;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Looper;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.simon.harmonichackernews.BuildConfig;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class NetworkComponent {
     private static volatile OkHttpClient okHttpClientInstance;
@@ -17,7 +23,20 @@ public class NetworkComponent {
         if (okHttpClientInstance == null) {
             synchronized (NetworkComponent.class) {
                 if (okHttpClientInstance == null) {
-                    okHttpClientInstance = new OkHttpClient();
+                    Interceptor userAgentInterceptor = new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request originalRequest = chain.request();
+                            Request requestWithUserAgent = originalRequest.newBuilder()
+                                    .header("User-Agent", "Harmonic-HN-Android/" + BuildConfig.VERSION_NAME + "/" + BuildConfig.BUILD_TYPE)
+                                    .build();
+                            return chain.proceed(requestWithUserAgent);
+                        }
+                    };
+
+                    okHttpClientInstance = new OkHttpClient.Builder()
+                            .addInterceptor(userAgentInterceptor)
+                            .build();
                 }
             }
         }
