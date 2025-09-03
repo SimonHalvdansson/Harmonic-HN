d5b40a3c73abc3e1bd94fb17d76af66b725150f7
arkon <arkon@users.noreply.github.com>
2025-09-03 16:12:09 -0400

    Use viewbinding in a few more activities

--- app/src/main/java/com/simon/harmonichackernews/WelcomeActivity.java
+++ app/src/main/java/com/simon/harmonichackernews/WelcomeActivity.java
@@ -6,86 +6,59 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
-import com.google.android.material.materialswitch.MaterialSwitch;
+import com.simon.harmonichackernews.databinding.ActivityWelcomeBinding;
 import com.simon.harmonichackernews.utils.ThemeUtils;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ThemeUtils.setupTheme(this);
+        ActivityWelcomeBinding binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
+        final View root = binding.getRoot();
+        setContentView(root);
 
-        setContentView(R.layout.activity_welcome);
-
-        ImageView favicon = findViewById(R.id.story_meta_favicon);
-        TextView index = findViewById(R.id.story_index);
-        TextView meta = findViewById(R.id.story_meta);
-        Button materialDaynightButton = findViewById(R.id.welcome_button_material_daynight);
-        Button materialDarkButton = findViewById(R.id.welcome_button_material_dark);
-        Button materialLightButton = findViewById(R.id.welcome_button_material_light);
-        Button darkButton = findViewById(R.id.welcome_button_dark);
-        Button grayButton = findViewById(R.id.welcome_button_gray);
-        Button blackButton = findViewById(R.id.welcome_button_black);
-        Button lightButton = findViewById(R.id.welcome_button_light);
-        Button whiteButton = findViewById(R.id.welcome_button_white);
-
+        ImageView favicon = binding.storyListItem.storyMetaFavicon;
         favicon.setImageResource(R.drawable.quanta);
 
-        MaterialSwitch thumbnailSwitch = findViewById(R.id.welcome_switch_thumbnails);
-        MaterialSwitch pointsSwitch = findViewById(R.id.welcome_switch_points);
-        MaterialSwitch indexSwitch = findViewById(R.id.welcome_switch_index);
-
-        thumbnailSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                favicon.setVisibility(b ? View.VISIBLE : View.GONE);
-                setBooleanSetting(compoundButton.getContext(), "pref_thumbnails", b);
-            }
+        binding.welcomeSwitchThumbnails.setOnCheckedChangeListener((@NonNull CompoundButton compoundButton, boolean b) -> {
+            favicon.setVisibility(b ? View.VISIBLE : View.GONE);
+            setBooleanSetting(compoundButton.getContext(), "pref_thumbnails", b);
         });
 
-        indexSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                index.setVisibility(b ? View.VISIBLE : View.GONE);
-                setBooleanSetting(compoundButton.getContext(), "pref_show_index", b);
-            }
+        binding.welcomeSwitchIndex.setOnCheckedChangeListener((@NonNull CompoundButton compoundButton, boolean b) -> {
+            binding.storyListItem.storyIndex.setVisibility(b ? View.VISIBLE : View.GONE);
+            setBooleanSetting(compoundButton.getContext(), "pref_show_index", b);
         });
 
-        pointsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                meta.setText((b ? "53 points • " : "") + "quantamagazine.org • 2 hrs");
-                setBooleanSetting(compoundButton.getContext(), "pref_show_points", b);
-            }
+        binding.welcomeSwitchPoints.setOnCheckedChangeListener((@NonNull CompoundButton compoundButton, boolean b) -> {
+            binding.storyListItem.storyMeta.setText((b ? "53 points • " : "") + "quantamagazine.org • 2 hrs");
+            setBooleanSetting(compoundButton.getContext(), "pref_show_points", b);
         });
 
-        View.OnClickListener buttonClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setSetting(view.getContext(), "pref_theme", (String) view.getTag());
-                restartActivity();
-            }
+        View.OnClickListener buttonClickListener = (View view) -> {
+            setSetting(view.getContext(), "pref_theme", (String) view.getTag());
+            restartActivity();
         };
 
-        materialDaynightButton.setOnClickListener(buttonClickListener);
-        materialDarkButton.setOnClickListener(buttonClickListener);
-        materialLightButton.setOnClickListener(buttonClickListener);
-        darkButton.setOnClickListener(buttonClickListener);
-        grayButton.setOnClickListener(buttonClickListener);
-        blackButton.setOnClickListener(buttonClickListener);
-        lightButton.setOnClickListener(buttonClickListener);
-        whiteButton.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonMaterialDaynight.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonMaterialDark.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonMaterialLight.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonDark.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonGray.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonBlack.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonLight.setOnClickListener(buttonClickListener);
+        binding.welcomeButtonWhite.setOnClickListener(buttonClickListener);
     }
 
     @SuppressLint("ApplySharedPref")
