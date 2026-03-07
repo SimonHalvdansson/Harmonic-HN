7c29f4e4c1216968b2468131e00556926e711518
SquirrelWave <SquirrelWave@proton.me>
2026-03-07 12:33:26 +0100

    Remove unused Uri import from SettingsActivity
    Rebased settings-redesign-multi-screen onto main (723b595).

--- app/src/main/java/com/simon/harmonichackernews/SettingsActivity.java
+++ app/src/main/java/com/simon/harmonichackernews/SettingsActivity.java
@@ -2,7 +2,6 @@ package com.simon.harmonichackernews;
 
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
