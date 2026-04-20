5e4c360b3d30b585b102989d4f9cfc0243c9ec8b
Simon Halvdansson <49042625+SimonHalvdansson@users.noreply.github.com>
2026-04-20 22:55:53 +0200

    Update Changelog.java

--- app/src/main/java/com/simon/harmonichackernews/utils/Changelog.java
+++ app/src/main/java/com/simon/harmonichackernews/utils/Changelog.java
@@ -7,6 +7,7 @@ public class Changelog {
                 "- Redesign settings screen into multiple levels (thanks SquirrelWave!)<br>" +
                 "- Added a new homescreen widget for stories (thanks SquirrelWave!)<br>" +
                 "- Fixed comment search scrolling when a matched comment is inside collapsed parents<br>" +
+                "- Fixed the \"Go back to comments\" setting on devices before Android 14<br>" +
                 "<br>" +
                 "<b>Version 2.2.6:</b><br>" +
                 "- Updated behavior when met with HN CAPTCHA<br>" +
