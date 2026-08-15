ec2327a2cc2aa8e073f0105bdadb165ab5e2b145
Simon Halvdansson <simonhalvdansson@gmail.com>
2026-08-15 11:38:03 +0200

    Fix default tint

--- app/src/main/java/com/simon/harmonichackernews/utils/PreviewImageTintUtils.kt
+++ app/src/main/java/com/simon/harmonichackernews/utils/PreviewImageTintUtils.kt
@@ -3,17 +3,12 @@ package com.simon.harmonichackernews.utils
 import android.content.Context
 import android.graphics.Color
 import com.google.android.material.color.MaterialColors
-import com.simon.harmonichackernews.R
 
 /** Android cache/theme adapter around the shared palette extraction and tint-state policy. */
 object PreviewImageTintUtils {
     fun getTintBaseColor(context: Context): Int = MaterialColors.getColor(
         context,
-        R.attr.storyCardBackgroundColor,
-        MaterialColors.getColor(
-            context,
-            com.google.android.material.R.attr.colorSurfaceContainerHigh,
-            Color.TRANSPARENT,
-        ),
+        com.google.android.material.R.attr.colorSurfaceContainerHigh,
+        Color.TRANSPARENT,
     )
 }
